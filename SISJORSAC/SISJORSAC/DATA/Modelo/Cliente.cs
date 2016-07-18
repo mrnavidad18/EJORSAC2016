f0dad7cffdce10d15866502ca10ed4010e47faed
Armando <jeffant.13@gmail.com>
2016-07-17 19:09:30 -0500

    s

--- SISJORSAC/SISJORSAC/DATA/Modelo/Cliente.cs
+++ SISJORSAC/SISJORSAC/DATA/Modelo/Cliente.cs
@@ -9,26 +9,26 @@ namespace SISJORSAC.DATA.Modelo
     class Cliente
     {
 
-        public int COD_CLI { get; set; }
-        public int NOMBRES { get; set; }
-        public int AP_PATERNO { get; set; }
-        public int AP_MATERNO { get; set; }
+                public int COD_CLI { get; set; }
+                public int NOMBRES { get; set; }
+                public int AP_PATERNO { get; set; }
+                public int AP_MATERNO { get; set; }
 
-        public int DNI { get; set; }
-        public int RUC { get; set; }
-        public int RAZON_SOCIAL { get; set; }
-        public int DIRECCION { get; set; }
-        public int DEPARTAMENTO { get; set; }
-        public int PROVINCIA { get; set; }
+                public int DNI { get; set; }
+                public int RUC { get; set; }
+                public int RAZON_SOCIAL { get; set; }
+                public int DIRECCION { get; set; }
+                public int DEPARTAMENTO { get; set; }
+                public int PROVINCIA { get; set; }
 
-        public int DISTRITO { get; set; }
+                public int DISTRITO { get; set; }        
+                public int TEL_FIJO_CASA { get; set;}
+                public int TEL_FIJO_OFICINA { get; set; }
+                public int CELULAR { get; set; }
+                public int EMAIL { get; set; }
 
-        public int MyProperty { get; set; }
-        public int MyProperty { get; set; }
-        public int TEL_FIJO_CASA { get; set;}
-
-       public int TEL_FIJO_OFICINA { get; set; }
-       public int CELULAR { get; set; }
-        public int EMAIL { get; set; }
+                public int OBSERVACIONES { get; set; }
+                public int TIPO_CLIE { get; set; }
+    
     }
 }
