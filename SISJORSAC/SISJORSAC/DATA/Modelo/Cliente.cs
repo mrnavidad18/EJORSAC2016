1fcf9268dc0c6fae58efa6f8375d8fdc9b6c41f0
Armando <jeffant.13@gmail.com>
2016-07-17 19:27:03 -0500

    2

--- SISJORSAC/SISJORSAC/DATA/Modelo/Cliente.cs
+++ SISJORSAC/SISJORSAC/DATA/Modelo/Cliente.cs
@@ -8,12 +8,10 @@ namespace SISJORSAC.DATA.Modelo
 {
     class Cliente
     {
-
                 public int COD_CLI { get; set; }
                 public int NOMBRES { get; set; }
                 public int AP_PATERNO { get; set; }
                 public int AP_MATERNO { get; set; }
-
                 public int DNI { get; set; }
                 public int RUC { get; set; }
                 public int RAZON_SOCIAL { get; set; }
@@ -26,9 +24,7 @@ namespace SISJORSAC.DATA.Modelo
                 public int TEL_FIJO_OFICINA { get; set; }
                 public int CELULAR { get; set; }
                 public int EMAIL { get; set; }
-
                 public int OBSERVACIONES { get; set; }
-                public int TIPO_CLIE { get; set; }
-    
+                public int TIPO_CLIE { get; set; }    
     }
 }
---
+++ SISJORSAC/SISJORSAC/DATA/Modelo/Contrato.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISJORSAC.DATA.Modelo
+{
+    class Contrato
+    {
+        public int COD_CONTRATO { get; set; }
+        public int NRO_CONTRATO { get; set; }
+        public int FECHA_CONTRATO { get; set; }
+        public Cliente cliente { get; set; }
+        public int RESPONSABLE_NOMBRE { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+
+
+    }
+}
