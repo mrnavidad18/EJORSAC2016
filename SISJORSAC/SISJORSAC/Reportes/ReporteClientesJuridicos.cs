05bc0a35f28ecd965e2ae7e865193dda0e72593d
rhuaman88 <ricardohf.21@gmail.com>
2016-07-20 23:54:20 -0500

    reportes

---
+++ SISJORSAC/SISJORSAC/Reportes/ReporteClientesJuridicos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SISJORSAC.Reportes
+{
+    public partial class ReporteClientesJuridicos : Form
+    {
+        public ReporteClientesJuridicos()
+        {
+            InitializeComponent();
+        }
+
+        private void ReporteClientesJuridicos_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
---
+++ SISJORSAC/SISJORSAC/Reportes/Tipo de Clientes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SISJORSAC.Reportes
+{
+    public partial class Tipo_de_Clientes : Form
+    {
+        public Tipo_de_Clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReporteClientes_Click(object sender, EventArgs e)
+        {
+            ReporteClientesNaturales rn = new ReporteClientesNaturales();
+            ReporteClientesJuridicos rj = new ReporteClientesJuridicos();
+
+            int op = cboClienteReporte.SelectedIndex;
+            if(op ==0){
+                MessageBox.Show("Elija un Tipo Cliente");
+            }
+
+            if (op == 1)
+            {
+                rn.ShowDialog();
+            }
+
+            if (op == 2)
+            {
+                rj.ShowDialog();
+            }
+        }
+    }
+}
